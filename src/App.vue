d80ce507246c1f49568c65d119f1b5c98be8f469
Elam Nabi <elam@nabi.nu>
2020-03-09 11:39:33 +0100

    Fixed the Lightbox

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <template>
-  <div></div>
+  <div>
+    <Lightbox />
+    <Gallery />
+  </div>
 </template>
 
 <script>
+import Lightbox from "@/components/Lightbox.vue";
+import Gallery from "@/components/Gallery.vue";
+
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    Lightbox,
+    Gallery
+  }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css?family=Poppins&display=swap");
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+
+  font-family: "Poppins", sans-serif;
+}
+</style>
